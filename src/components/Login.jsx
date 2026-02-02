c0f0a10df3e5608550c87317df0816a1d3c4b8b8
Suraj Kumar <20203048.it@gmail.com>
2026-02-02 20:51:30 +0530

    setup redux store

---
+++ src/components/Login.jsx
@@ -0,0 +1,83 @@
+import axios from 'axios';
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
+import { useNavigate } from 'react-router-dom';
+import { BASE_URL } from '../utils/constants';
+
+
+const Login = () => {
+  const [emailId, setEmailId] = useState("virat99@tcs.com");
+  const [password, setPassword] = useState("Virat@876");
+   const dispatch=useDispatch();
+   const usenavigate=useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(BASE_URL+"/login", {
+        emailId,
+        password,
+      },{withCredentials:true});
+      console.log(res.data)
+      dispatch(addUser(res.data));
+     return usenavigate("/")
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-base-200">
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="text-2xl font-bold text-center mb-4">
+            Welcome Back 👋
+          </h2>
+
+          {/* Email */}
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email address</span>
+            </label>
+            <input
+              type="email"
+              className="input input-bordered bg-base-200 px-2"
+              value={emailId}
+              onChange={(e) => setEmailId(e.target.value)}
+            />
+          </div>
+
+          {/* Password */}
+          <div className="form-control mt-2">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              className="input input-bordered bg-base-200 px-2"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          {/* Button */}
+          <div className="form-control mt-6">
+            <button
+              className="btn btn-primary w-full bg-blue-500"
+              onClick={handleLogin}
+            >
+              Login
+            </button>
+          </div>
+
+          {/* Extra text */}
+          <p className="text-center text-sm mt-4 text-gray-500 ">
+            Don’t have an account? <span className="link link-primary">Sign up</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login
