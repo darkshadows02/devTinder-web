7c46c753899e7d2a5e5b4706d0b274373997675a
Suraj Kumar <20203048.it@gmail.com>
2026-02-10 10:05:37 +0530

    accept/reject connection request

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -1,29 +1,39 @@
 import axios from 'axios'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { BASE_URL } from '../utils/constants'
 import { useDispatch, useSelector } from 'react-redux'
-import { addRequests } from '../utils/requestSlice'
+import { addRequests, removeRequest } from '../utils/requestSlice'
 
 const Requests = () => {
   const requests=useSelector((store)=> store.requests);
   const dispatch=useDispatch();
-    const featchRequests= async()=>{
+     const featchRequests= async()=>{
            try{
               const res=await axios.get(BASE_URL+"/user/requests/received", {
                 withCredentials:true,
               })
-              console.log(res)
+               
                dispatch(addRequests(res.data.data));
 
            }catch(err){
 
            }
     }
+    const reviewRequest=async(status, _id)=>{
+      try{
+        const res=await axios.post(BASE_URL+"/request/review/"+status+"/"+_id, {}, {
+          withCredentials:true
+        } )
+        dispatch(removeRequest(_id));
+      }catch(err){
+
+      }
+    }
     useEffect(()=>{
       featchRequests();
     }, [])
     if(!requests) return ;
-    if(requests.length===0) return <h1> Request Not Found</h1>;
+    if(requests.length===0) return <h1 className='text-center mt-5 '> Request Not Found</h1>;
   return (
     <div className='text-center my-10'>
         <h1 className='text-bold text-2xl'> Connections Request</h1> 
@@ -42,8 +52,8 @@ const Requests = () => {
                   <p>{about}</p>
                   </div>
                   <div>
-                    <button className='btn btn-primary mx-2 bg-red-500 px-1'>Reject</button>
-                    <button className='btn btn-secondary mx-2 bg-blue-500 px-1'>Accept</button>
+                    <button className='btn btn-primary mx-2 bg-red-500 px-1' onClick={()=>reviewRequest("rejected", request._id)}>Reject</button>
+                    <button className='btn btn-secondary mx-2 bg-blue-500 px-1' onClick={()=>reviewRequest("accepted", request._id)}>Accept</button>
                     </div>
               </div>
 
