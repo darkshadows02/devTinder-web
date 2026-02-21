26606671caa76066e5cdb2f2201745bce3bce9b6
Suraj Kumar <20203048.it@gmail.com>
2026-02-21 12:32:20 +0530

    some changes in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div> 
         <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4 fixed bottom-0">
   <aside>
-    <p>Copyright © {new Date().getFullYear()} - Copyright © 2026 Suraj – All rights reserved. Developed by Suraj, Software Engineer.</p>
+    <p>Copyright © {new Date().getFullYear()} - Suraj – All rights reserved. Developed by Suraj, Software Engineer.</p>
   </aside>
 </footer>
     </div>
